6e028b8cbdea2ecd1c1deaf807c6f9c662c8126d
Matthew Hersh <mattmhersh@gmail.com>
2013-05-17 12:12:28 -0400

    Update

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var MyCtrl = function($scope) {
- $scope.items = [
-     { name: "Idiot IPA", price: 8.99, lastDrank: "2013-05-17" },
-     { name: "Inversion IPA", price: 6.99, lastDrank: "2013-05-17" } ]
+    $scope.items = [
+         { name: "Idiot IPA", price: 8.99, lastDrank: "2013-05-17" },
+         { name: "Inversion IPA", price: 6.99, lastDrank: "2013-05-17" } ]
+
+    $scope.pluralizer = {
+        0 : "No beers!",
+        1 : "Only one left!",
+        other: "{} Beers in Fridge"
+    }
+
 }
+
