96047fcb678ca02a5043a3ffd3cd10931efa24e1
Matthew Hersh <mattmhersh@gmail.com>
2013-05-17 11:28:50 -0400

    Update

---
+++ app.js
@@ -0,0 +1,5 @@
+var MyCtrl = function($scope) {
+ $scope.items = [
+     { name: "Idiot IPA", price: 8.99, lastDrank: "2013-05-17" },
+     { name: "Inversion IPA", price: 6.99, lastDrank: "2013-05-17" } ]
+}
