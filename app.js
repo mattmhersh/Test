a282837622096c70ba2abe4ae3ad17d3d4b5f3b5
Matthew Hersh <mattmhersh@gmail.com>
2013-05-17 12:51:41 -0400

    Update

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ var MyCtrl = function($scope) {
     $scope.items = [
          { name: "Idiot IPA", price: 8.99, lastDrank: "2013-05-17" },
          { name: "Inversion IPA", price: 6.99, lastDrank: "2013-05-17" } ]
-
     $scope.pluralizer = {
         0 : "No beers!",
         1 : "Only one left!",
         other: "{} Beers in Fridge"
     }
-
     $scope.addBeer = function() {
         var newBeer = { name : $scope.name, price : $scope.price, lastDrank : new Date() };
         $scope.items.push(newBeer);
     }
-
+    $scope.removeItem = function(item) {
+        if (confirm("Remove this beer?")) {
+            $scope.items.splice($scope.items.indexOf(item),1);
+        }
+    }
 }
 
