760bc52a8953240cc0e513902da2f22332a391dc
Matthew Hersh <mattmhersh@gmail.com>
2013-05-17 19:08:46 -0400

    Fixed adding and deleting using mongolab. Had to override DB methods .

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
-var ngMongo = angular.module("ngMongo", ['ngResource'])
+// This is a module for cloud persistance in mongolab - https://mongolab.com
+var ngMongo = angular.module('ngMongo', ['ngResource']).
+    factory('Mongo', function ($resource) {
+        var database = $resource('https://api.mongolab.com/api/1/databases' +
+            '/matthersh/collections/Beers/:id',
+            { apiKey: 'kQCb_3UOG14wG_RZlSanbD-rPMoa7cgK' }, {
+                update: { method: 'PUT' }
+            }
+        );
 
-ngMongo.factory("Mongo", function($resource) {
-    return {
-        database : $resource("https://api.mongolab.com/api/1/databases/matthersh/collections/Beers/?apiKey=kQCb_3UOG14wG_RZlSanbD-rPMoa7cgK")
+        database.prototype.update = function (cb) {
+            return database.update({id: this._id.$oid},
+                angular.extend({}, this, {_id: undefined}), cb);
+        };
+
+        database.prototype.delete = function (cb) {
+            return database.remove({id: this._id.$oid}, cb);
+        };
+
+        return database;
+    });
+
+
+ngMongo.controller("ListCtrl", function ($scope, Mongo) {
+    $scope.items = Mongo.query({});
+    $scope.addDb = function () {
+        var dbName = $scope.newDbName;
+        if (dbName) {
+            var newDb = new Mongo({name: dbName});
+            newDb.$save();
+            $scope.items.push(newDb);
+        }
+    }
+
+    $scope.removeDb = function (db) {
+        if (confirm("Are you sure you would like to delete this?")) {
+            $scope.items.splice($scope.items.indexOf(db), 1);
+            db.delete(db);
+        }
     }
 });
 
-ngMongo.controller("ListCtrl", function($scope, Mongo) {
-    $scope.items = Mongo.database.query({});
-});
 
